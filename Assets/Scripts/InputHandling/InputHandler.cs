d09d53e59e9e334c76c308c640f6a86793c4ff61
Bryan Brouwer <bryanbrouwerus@gmail.com>
2025-06-28 15:37:57 +0200

    edit: added correct cleanup and easier binding handling

--- Assets/Scripts/InputHandling/InputHandler.cs
+++ Assets/Scripts/InputHandling/InputHandler.cs
@@ -1,7 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
 using InputHandling.InputStrategies;
 using Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace InputHandling
 {
@@ -10,8 +12,19 @@ namespace InputHandling
         [field: SerializeField] public InputStrategy InputStrategy {get; private set;}
         
         public PlayerMovement playerMovement;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        private void Start()
+        private IEnumerator _customLogicCoroutine;
+        private readonly List<(InputAction action, System.Action<InputAction.CallbackContext> callback)> _bindings = new ();
+
+        private void RegisterBinding(InputAction action, MoveDirection direction)
+        {
+            System.Action<InputAction.CallbackContext> callback = ctx => playerMovement.DirectionalMove(direction);
+            action.performed += callback;
+            action.Enable();
+
+            _bindings.Add((action, callback));
+        }
+        
+        private void OnEnable()
         {
             if (!InputStrategy)
             {
@@ -28,24 +41,48 @@ namespace InputHandling
                     if (InputStrategy.GetInputLogic(playerMovement) is InputBindings bindings)
                     {
                         Debug.Log("Using bindings");
-                        //Can make this more dynamic by changing the struct to contain a dictionary of actions using the action as key and the direction as value
-                        bindings.moveLeftAction.performed += context => playerMovement.DirectionalMove(MoveDirection.Left);
-                        bindings.moveRightAction.performed += context => playerMovement.DirectionalMove(MoveDirection.Right);
-                        bindings.moveForwardAction.performed += context => playerMovement.DirectionalMove(MoveDirection.Front);
-                        bindings.moveBackwardAction.performed += context => playerMovement.DirectionalMove(MoveDirection.Back);
+                        //TODO Can make this more dynamic by also changing the struct contained in the InputStrategy, but for now this if fine for the easier management
+                        _bindings.Clear();
                         
-                        bindings.moveLeftAction.Enable();
-                        bindings.moveRightAction.Enable();
-                        bindings.moveForwardAction.Enable();
-                        bindings.moveBackwardAction.Enable();
+                        RegisterBinding(bindings.moveLeftAction, MoveDirection.Left);
+                        RegisterBinding(bindings.moveRightAction, MoveDirection.Right);
+                        RegisterBinding(bindings.moveForwardAction, MoveDirection.Front);
+                        RegisterBinding(bindings.moveBackwardAction, MoveDirection.Back);
                     }
                     break;
                 case InputStrategyType.CustomLogic:
                     if (InputStrategy.GetInputLogic(playerMovement) is IEnumerator coroutine)
                     {
                         Debug.Log("Using custom logic");
+                        _customLogicCoroutine = coroutine;
                         StartCoroutine(coroutine);
                     }
+                    break;
+                default:
+                    throw new System.ArgumentOutOfRangeException();
+            }
+        }
+
+        private void OnDisable()
+        {
+            switch (InputStrategy.InputStrategyType)
+            {
+                case InputStrategyType.InputBindings:
+                    if (InputStrategy.GetInputLogic(playerMovement) is InputBindings bindings)
+                    {
+                        foreach (var (action, callback) in _bindings)
+                        {
+                            action.performed -= callback;
+                            action.Disable();
+                        }
+
+                        _bindings.Clear();
+                    }
+                    break;
+                case InputStrategyType.CustomLogic:
+                    if (_customLogicCoroutine == null)
+                        return;
+                    StopCoroutine(_customLogicCoroutine);
                     break;
                 default:
                     throw new System.ArgumentOutOfRangeException();
